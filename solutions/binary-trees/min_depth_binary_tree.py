d3baedd01f768edfa0fb21a1c36fbf810484aa9a
Roy Chebaclo <rmchebaclo@Roys-MBP.attlocal.net>
2024-07-19 20:13:53 -0500

    min depth bfs

---
+++ solutions/binary-trees/min_depth_binary_tree.py
@@ -0,0 +1,23 @@
+from tree_node import TreeNode
+from collections import deque
+class Solution:
+    # iterative bfs
+    # time complexity: O(n)
+    # space complexity: O(w) width = n in worst case
+    def minDepth(self, root: TreeNode) -> int:
+        depth = 0
+        queue = deque()
+        if root:
+            queue.append(root)
+        
+        while len(queue) > 0:
+            depth += 1
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                if not curr.left and not curr.right:
+                    return depth
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+        return depth
