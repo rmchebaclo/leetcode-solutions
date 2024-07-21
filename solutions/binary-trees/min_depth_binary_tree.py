c6f5cdd7289d79a7cfb72339ad247e5e2a75c25e
Roy Chebaclo <rmchebaclo@Roys-MacBook-Pro.local>
2024-07-21 13:18:43 -0500

    recursive dfs solution to min depth

--- solutions/binary-trees/min_depth_binary_tree.py
+++ solutions/binary-trees/min_depth_binary_tree.py
@@ -21,3 +21,15 @@ class Solution:
                 if curr.right:
                     queue.append(curr.right)
         return depth
+    
+    # recursive dfs
+    # time complexity: O(n)
+    # space complexity: O(h), h = logn in balanced tree, h = n in worst case
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if  not root.right:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.left) + 1, self.minDepth(root.right) + 1)
