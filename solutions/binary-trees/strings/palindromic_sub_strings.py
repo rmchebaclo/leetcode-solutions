119a30d776fa65420aec11070bd014a9062f2adf
Roy Chebaclo <rmchebaclo@Roys-MacBook-Pro.local>
2024-08-03 22:44:50 -0500

    optimal solution to count palindromic substrings

--- solutions/binary-trees/strings/palindromic_sub_strings.py
+++ solutions/binary-trees/strings/palindromic_sub_strings.py
@@ -18,3 +18,26 @@ class Solution:
         if len(substring) <= 1:
             return True
         return substring[0] == substring[-1] and self.isPalindrome(substring[1:-1])
+    
+    # optimal solution
+    # for each character in s treat it like the middle of the substring and expand around it
+    # stop expanding when left or right pointer goes out of bounds
+    # for odd length palindromes start with left and pointer equal to eachother then expand around it
+    # for even length palindromes start with right pointer being 1 greater than left
+    # time complexity: O(n^2)
+    # space complexity: O(1)
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        i = 0
+        for i in range(len(s)):
+            res += self.countPalindromes(s, i, i)
+            res += self.countPalindromes(s, i, i + 1)
+        return res
+    
+    def countPalindromes(self, s, l, r):
+        counter = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            counter += 1
+            l -= 1
+            r += 1
+        return counter
